c221da56f5885365045da30d95ae907cd88a9598
Alexander Schüler <36029095+Headlex@users.noreply.github.com>
2026-02-05 09:25:48 +0100

    feat: add config.fish

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,12 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+zoxide init fish | source
+source (/usr/bin/starship init fish --print-full-init | psub)
+
+function ls --wraps=eza
+    eza --icons --group-directories-first -1  $argv
+end
+
+function ll --wraps=eza
+    eza -l --icons --group-directories-first --git --smart-group -h --git-repos -1 -a $argv
+end
